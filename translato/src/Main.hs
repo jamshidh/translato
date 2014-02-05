e5a13b41f364f7e8bb21d6a2aa18b7d9b65eb90b
jim <jim>
2014-02-05 11:44:04 -0800

    Adding code to get this to work.

--- translato/src/Main.hs
+++ translato/src/Main.hs
@@ -20,11 +20,34 @@ main = do
 
     resourceDir <- getDataFileName ""
 
-    readProcess "parser" ["parse", filename] ""
-        >>= doXsltXform (shimDir </> "detailsSummaryEvents" </> "addDetailsSummaryEvents.xsl")
-        >>= doXsltXform (resourceDir </> "reorganize.xsl")
+    translators <-
+        map doXsltXform <$>
+            (filterExists
+                =<< map (</> "translate.xsl")
+                <$> map (shimDir </>)
+                <$> getDirectoryContents shimDir)
+
+    putStrLn ("There are " ++ show (length translators) ++ " translators")
+
+    --This chains together functions in a list [a, b, c, d]
+    --using (>>=), like this ">>= a >>= b >>= c >>= d"
+    let allTranslatorsTogether =
+            if length translators == 0
+                then id
+                else foldl1 (.) $ (=<<) <$> translators
+
+    (allTranslatorsTogether $ readProcess "parser" ["parse", filename] "")
+        >>= (doXsltXform (resourceDir </> "reorganize.xsl"))
         >>= readProcess "parser" ["generate", "html5"]
         >>= putStrLn
+
+filterExists::[FilePath]->IO [FilePath]
+filterExists [] = return []
+filterExists (first:rest) = do
+    exists <- doesFileExist first
+    filteredRest <- filterExists rest
+    return $
+        if exists then first:filteredRest else filteredRest
 
 doXsltXform::FilePath->String->IO String
 doXsltXform xsltFilePath input = do

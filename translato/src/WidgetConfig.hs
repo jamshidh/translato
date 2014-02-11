13c9db5a59c3f944a567b76ea571d5c8a9337f40
jim <jim>
2014-02-10 18:22:54 -0800

    Adding code to check for version of browser and decide which shims to use.

--- translato/src/WidgetConfig.hs
+++ translato/src/WidgetConfig.hs
@@ -1,23 +1,29 @@
 {-# LANGUAGE OverloadedStrings, NoMonomorphismRestriction #-}
+{-# OPTIONS_GHC -Wall #-}
 
 module WidgetConfig (
     WidgetConfig(..),
+    Browser(..),
+    Version,
+    VersionRange(..),
     getConfigFile
 ) where
 
+import Control.Exception.Base
 import qualified Data.ByteString.Lazy.Char8 as B
+import Data.Conduit
 import Data.Functor
-import Data.Maybe
 import qualified Data.Text as T
 import qualified Data.Text.Lazy as TL
+import System.FilePath
 import Text.XML.Stream.Parse
+import Data.XML.Types
 import qualified Text.XML as XML
 
-import Control.Monad.Trans.Resource
-import Data.Conduit (($$))
-import Data.Text (Text, unpack)
-import Text.XML.Stream.Parse
+--import Control.Monad.Trans.Resource
+
 import Paths_translato
+
 
 import GrammarTools
 import Parser
@@ -25,76 +31,90 @@ import Parser
 data WidgetConfig =
     WidgetConfig {
         browsers::[Browser]
-    }
+    } deriving (Show)
 
 data Browser = IE VersionRange | Mozilla VersionRange | Webkit VersionRange deriving (Show)
 
-data VersionRange = Exact Version | LowerBound Version | UpperBound Version | Range Version Version deriving (Show)
+data VersionRange = AllVersions | Exact Version | LowerBound Version | UpperBound Version | Range Version Version deriving (Show)
 
 type Version = [Int]
 
-parseConfig = tagNoAttr "config" parseBrowsers
+parseConfig::Sink Event (Either SomeException) (Maybe WidgetConfig)
+parseConfig = tagNoAttr "config" $ do
+    browserList <- parseBrowsers
+    case browserList of
+        Just x -> return WidgetConfig { browsers=x }
+        _ -> return WidgetConfig { browsers=[] }
 
+parseBrowsers::Sink Event (Either SomeException) (Maybe [Browser])
 parseBrowsers = tagNoAttr "browsers" $ many parseBrowser
 
+parseBrowser::ConduitM Event o (Either SomeException) (Maybe Browser)
 parseBrowser = choose [parseIE, parseMozilla, parseWebkit]
 
+parseIE::ConduitM Event o (Either SomeException) (Maybe Browser)
 parseIE = tagNoAttr "ie" $ do
-    Just versionRange <- parseVersionRange
-    return $ IE versionRange
+    maybeVersionRange <- parseVersionRange
+    case maybeVersionRange of
+        Just versionRange -> return $ IE versionRange
+        Nothing -> return $ IE AllVersions
 
+parseMozilla::ConduitM Event o (Either SomeException) (Maybe Browser)
 parseMozilla = tagNoAttr "mozilla" $ do
-    Just versionRange <- parseVersionRange
-    return $ Mozilla versionRange
+    maybeVersionRange <- parseVersionRange
+    case maybeVersionRange of
+        Just versionRange -> return $ Mozilla versionRange
+        Nothing -> return $ Mozilla AllVersions
 
+parseWebkit::ConduitM Event o (Either SomeException) (Maybe Browser)
 parseWebkit = tagNoAttr "webkit" $ do
-    Just versionRange <- parseVersionRange
-    return $ Webkit versionRange
+    maybeVersionRange <- parseVersionRange
+    case maybeVersionRange of
+        Just versionRange -> return $ Webkit versionRange
+        Nothing -> return $ Webkit AllVersions
 
+parseVersionRange::ConduitM Event o (Either SomeException) (Maybe VersionRange)
 parseVersionRange = choose [parseUpperBound, parseLowerBound, parseExact, parseRange]
 
+parseUpperBound::ConduitM Event o (Either SomeException) (Maybe VersionRange)
 parseUpperBound = tagNoAttr "upperBound" $ do
-    Just version <- parseVersion
-    return $ UpperBound version
+    ver <- force "version required" $ parseVersion
+    return $ UpperBound ver
 
+parseLowerBound::ConduitM Event o (Either SomeException) (Maybe VersionRange)
 parseLowerBound = tagNoAttr "lowerBound" $ do
-    Just version <- parseVersion
-    return $ LowerBound version
+    ver <- force "version required" $ parseVersion
+    return $ LowerBound ver
 
+parseExact::ConduitM Event o (Either SomeException) (Maybe VersionRange)
 parseExact = tagNoAttr "exact" $ do
-    Just version <- parseVersion
-    return $ Exact version
+    ver <- force "version range required" $ parseVersion
+    return $ Exact ver
 
+parseRange::ConduitM Event o (Either SomeException) (Maybe VersionRange)
 parseRange = tagNoAttr "exact" $ do
-    Just (LowerBound lower) <- parseLowerBound
-    Just (UpperBound upper) <- parseUpperBound
+    LowerBound lower <- force "lower bound required" $ parseLowerBound
+    UpperBound upper <- force "upper bound required" $ parseUpperBound
     return $ Range lower upper
 
+parseVersion::ConduitM Event o (Either SomeException) (Maybe Version)
 parseVersion = tagNoAttr "version" $ many parseInteger
 
+parseInteger::ConduitM Event o (Either SomeException) (Maybe Int)
 parseInteger = tagNoAttr "integer" $ do
     intValue <- content
     return $ read $ T.unpack intValue
 
-xml2WidgetConfig::XML.Document->WidgetConfig
-xml2WidgetConfig doc =
-  WidgetConfig {
-    browsers=[]
-  }
-
-getConfigFile::FilePath->IO XML.Document
-getConfigFile configFileName = do
+getConfigFile::FilePath->IO WidgetConfig
+getConfigFile shimDir = do
     grammar <- loadGrammarAndSimplifyForParse =<< getDataFileName "config.spec"
-    maybeDoc <- XML.parseText XML.def <$> TL.pack <$> createParser grammar <$> readFile configFileName
+    maybeDoc <- XML.parseText XML.def <$> TL.pack <$> createParser grammar <$> readFile (shimDir </> "config")
     case maybeDoc of
         Right doc -> do
-            let content = TL.unpack (XML.renderText XML.def doc)
-            putStrLn content
-            config <- runResourceT $
-                parseLBS def (B.pack content) $$ force "config required" parseConfig
-            print config
-
-    return $
-        case maybeDoc of
-            Right doc -> doc
-            Left err -> error $ show err
+            let doc' = B.pack $ TL.unpack $ XML.renderText XML.def doc
+            --runResourceT $
+            case parseLBS def doc' $$ force "config required" parseConfig of
+                Left err ->
+                    error $ ("There is an error: " ++ show err ++ ", file content is " ++ show (XML.renderText XML.def doc))
+                Right config -> return config
+        Left err -> error $ show err
--- translato/src/WidgetLibGenerator.hs
+++ translato/src/WidgetLibGenerator.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE OverloadedStrings #-}
 {-# OPTIONS_GHC -Wall #-}
 
 module WidgetLibGenerator (
@@ -6,15 +7,17 @@ module WidgetLibGenerator (
 ) where
 
 import Control.Monad
-import Data.Foldable hiding (concat)
+import qualified Data.ByteString.UTF8 as B
+import Data.Foldable hiding (concat, and, or)
 import Data.Functor
 import Data.List
 import Data.String
-import qualified Data.Text.Lazy as T
+import qualified Data.Text as T
 import System.Directory
 import System.FilePath
 import System.FilePath.Find as Find hiding (fold)
 import qualified Text.XML as XML
+import Web.UAParser
 
 import Format
 import Widget
@@ -33,15 +36,35 @@ getShimNames shimDir =
         <$> filter (not . ("." `isPrefixOf`))
         <$> getDirectoryContents shimDir
 
-filterIneligibleShims::String->[FilePath]->IO [FilePath]
-filterIneligibleShims _ [] = return []
-filterIneligibleShims userAgent (first:rest) = do
-    configFile <- getConfigFile (first </> "config")
-    putStrLn "----------------"
-    putStrLn $ T.unpack (XML.renderText XML.def configFile)
-    putStrLn "----------------"
-    remainder <- filterIneligibleShims userAgent rest
-    return $ first:remainder
+versionInRange::Version->VersionRange->Bool
+versionInRange v1 (Exact v2) = v1 == v2
+versionInRange v1 (LowerBound v2) = v1 >= v2
+versionInRange v1 (UpperBound v2) = v1 <= v2
+versionInRange v1 (Range v2 v3) = v1 >= v2 && v1 <= v3
+
+
+uaResultToVersion::UAResult->Version
+uaResultToVersion UAResult{uarV1=Just v1, uarV2=Just v2, uarV3=Just v3} =
+    read <$> T.unpack <$> [v1, v2, v3]
+uaResultToVersion UAResult{uarV1=Just v1, uarV2=Just v2, uarV3=Nothing} =
+    read <$> T.unpack <$> [v1, v2]
+uaResultToVersion UAResult{uarV1=Just v1, uarV2=Nothing, uarV3=Nothing} =
+    read <$> T.unpack <$> [v1]
+uaResultToVersion uaResult =
+    error ("Error: An odd parameter was passed to uaResultToVersion: " ++ show uaResult)
+
+uaInBrowserRange::UAResult->Browser->Bool
+uaInBrowserRange uaResult@UAResult{uarFamily="Firefox"} (Mozilla versionRange) =
+    versionInRange (uaResultToVersion uaResult) versionRange
+uaInBrowserRange uaResult@UAResult{uarFamily="Chrome"} (Webkit versionRange) =
+    versionInRange (uaResultToVersion uaResult) versionRange
+uaInBrowserRange _ _ = False
+
+isShimEligible::UAResult->FilePath->IO Bool
+isShimEligible userAgent shimFilePath = do
+    config <- getConfigFile shimFilePath
+    putStrLn $ show config
+    return $ or $ uaInBrowserRange userAgent <$> browsers config
 
 getWidgetNames::FilePath->IO [String]
 getWidgetNames shimDir = do
@@ -52,9 +75,11 @@ getDirectoryFilePathContents::FilePath->IO [FilePath]
 getDirectoryFilePathContents x = map (x </>) <$> getDirectoryContents x
 
 getWidgetLibContent::FilePath->String->String->IO (Maybe String, Maybe String)
-getWidgetLibContent shimDir widgetName userAgent = do
-    putStrLn userAgent
-    neededShims <- filterIneligibleShims userAgent =<< getShimNames shimDir
+getWidgetLibContent shimDir userAgentString widgetName = do
+    uaParser <- loadUAParser
+    let Just userAgent = parseUA uaParser $ B.fromString userAgentString
+    neededShims <- filterM (isShimEligible userAgent) =<< getShimNames shimDir
+    putStrLn ("Needed shims: " ++ show (takeBaseName <$> neededShims))
     widgetFiles <-
         filter ((".widget" ==) . takeExtension)
             <$> concat

04558c0501201e33f9bbc885e2ab0418fb634839
jim <jim>
2014-02-26 19:02:02 -0800

    Cleanup, and adding operator name for operators that are actually words (like "in" in javascript, which is used like this- "if (x in y)....").

--- parser/src/OperatorNames.hs
+++ parser/src/OperatorNames.hs
@@ -1,65 +1,16 @@
------------------------------------------------------------------------------
---
--- Module      :  OperatorNames
--- Copyright   :
--- License     :  AllRightsReserved
---
--- Maintainer  :
--- Stability   :
--- Portability :
---
--- |
---
------------------------------------------------------------------------------
+{-# OPTIONS_GHC -Wall #-}
 
 module OperatorNames (
-    --OperatorSymbol,
-    --expandOperators,
     symbol2Name,
     op2Name
 ) where
 
+import Data.Char
 import Data.List
-import Data.Map hiding (map, foldl, filter)
-import Data.Text hiding (concat, map, filter, length)
 
 import Grammar
 
---import Debug.Trace
-
---import Grammar
-
-{--
-operatorList2Expressions::(RuleName, [OperatorSymbol])->[(RuleName, Sequence)]
-operatorList2Expressions (name, []) = []
-operatorList2Expressions (name, (o1:rest)) =
-    (op2Name o1, [SepBy2 [Or ((map (\x -> [Link (op2Name x)]) rest) ++ [[Link ("#" ++ name ++ "_terminals")]])]
-            [TextMatch o1]])
-        :operatorList2Expressions (name, rest)
-
-operatorFreeRules::[(RuleName, Sequence)]->Map RuleName [OperatorSymbol]->[(RuleName, Sequence)]
-operatorFreeRules rules opDefinitions = filter (\(ruleName, _) -> notMember ruleName opDefinitions) rules
-
-addTerminals::[(RuleName, Sequence)]->Map RuleName [OperatorSymbol]->[(RuleName, Sequence)]
-addTerminals rules opDefinitions =
-    (map (\(n, e) -> ("#" ++ n ++ "_terminals", e)) operatorRules)
-        ++ (map (\(n, e) -> (n, [Link ("#" ++ n ++ "_terminals")])) operatorRules)
-    where operatorRules = filter (\(ruleName, _) -> member ruleName opDefinitions) rules
-
-operatorExpressions::Grammar->[(RuleName, Sequence)]
-operatorExpressions g = concat (map operatorList2Expressions (toList $ operatorDefinitions g))
-
-expandOperators::Grammar->Grammar
-expandOperators g = g {
-        elementRules = operatorFreeRules (elementRules g) (operatorDefinitions g)
-            ++ addTerminals (elementRules g) (operatorDefinitions g)
-            ++ operatorExpressions g,
-        operatorDefinitions = fromList []
-        }--}
-
-{--op2Name::OperatorSymbol->String
-op2Name (TextMatch s:rest) = rawOp2Name s ++ (op2Name rest)
-op2Name (x:rest) = op2Name rest--}
+--import JTrace
 
 symbol2Name::Sequence->String
 symbol2Name (TextMatch text _:rest) = op2Name text ++ symbol2Name rest
@@ -80,4 +31,5 @@ op2Name "<" = "lessThan"
 op2Name ">" = "greaterThan"
 op2Name "<=" = "lessThanOrEquals"
 op2Name ">=" = "greaterThanOrEquals"
+op2Name x | all isAlpha x = x
 op2Name x = error ("Unknown operator in op2Name: \'" ++ x ++ "'")

567db96702f184846761403485a77e136db92102
jim <jim>
2014-03-05 17:28:49 -0800

    Adding some functions that I need....  These really need to be placed in their own shim, and I will fix this later....

--- html5/shims/addEventHandler/lib/addEventHandler.js
+++ html5/shims/addEventHandler/lib/addEventHandler.js
@@ -1,19 +1,73 @@
 
+Object.getPrototypeOf = function(obj) {
+    return obj.constructor.prototype;
+};
 
-function addEventListenerFunc(x, y, z) {
-    alert("Setting event listener: " + x + ", " + y + ", " + z);
+//modified from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter#Compatibility
+Array.prototype.filter = function(fun /*, thisArg */)
+{
+    var res = [];
+    var thisArg = arguments.length >= 2 ? arguments[1] : void 0;
+    for (var i = 0; i < this.length; i++)
+    {
+	if (i in this)
+	{
+            var val = this[i];
+            if (fun.call(thisArg, val, i, this))
+		res.push(val);
+	}
+    }
+    
+    return res;
+};
+
+function isCustomEvent(type) {
+    if (type === "click" ||
+	type === "load") return false;
+
+    return true;
 }
 
-function removeEventListenerFunc(x, y, z) {
-    alert("Removing event listener");
+Element.prototype.addEventListener = function (type, listener, useCapture) {
+    //TODO- figure out how to deal with useCapture
+    if (isCustomEvent(type)) {
+	this[type] = 0;
+
+	this.attachEvent("onpropertychange", function(event) {
+            if (event.propertyName == type) {
+		listener(event);
+            }
+	});
+    }
+    else {
+	this.attachEvent("on" + type, listener);
+    }
+
 }
 
-//HTMLDocument.prototype.addEventListener = addEventListenerFunc;
-//HTMLDocument.prototype.removeEventListener = removeEventListenerFunc;
+window.addEventListener = Element.prototype.addEventListener;
 
-//Element.prototype.addEventListener = addEventListenerFunc;
-//Element.prototype.removeEventListener = removeEventListenerFunc;
+//window.removeEventListener = function() {
+//this.detachEvent("onpropertychange", arguments.callee);
+//}
 
-window.addEventListener = addEventListenerFunc;
-window.removeEventListener = removeEventListenerFunc;
+Element.prototype.dispatchEvent = function (event) {
+    //alert("dispatchEvent: event = " + event);
+    if (isCustomEvent(event.type)) {
+	this[event.type]++;
+    }
+    else {
+	this.fireEvent("on" + event.type, event);
+    }
+}
 
+document.createEvent = function (type) {
+    //alert("createEvent: type = " + type);
+    return {"qqqq": "qqqq", 
+	    initEvent: function(type, bubbles, cancelable) { 
+		//alert("initEvent: type=" + type + ", bubbles=" + bubbles + ", cancelable=" + cancelable); 
+		//alert("creating: " + type);
+		return {qqqq:"qqqq"};
+	    } 
+	   };
+}

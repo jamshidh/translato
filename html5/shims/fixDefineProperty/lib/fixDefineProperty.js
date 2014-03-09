6963ed6bcd041dae71e395d1b22189f537e1bce9
jim <jim>
2014-03-08 20:37:34 -0800

    I am putting the code that turns off defineProperty in another event handler, so that the fact that the handler is turned off doesn't affect the current code.

--- html5/shims/fixDefineProperty/lib/fixDefineProperty.js
+++ html5/shims/fixDefineProperty/lib/fixDefineProperty.js
@@ -15,15 +15,19 @@ Object.fixedDefineProperty = function (obj, prop, descriptor) {
     //Note that this trick doesn't seem to work with getAttribute (else we wouldn't need the closure at all),
     //because defineProperty/delete property resets the value of the property to null.
     function setRealValue(obj, prop, value) {
-	var descriptor = Object.getOwnPropertyDescriptor(obj, prop);
-	delete obj[prop]; 
-	originalSetAttribute(prop, value);
-	obj.className=obj.className;
-	//Without the "setTimeout", the style changes don't apply correctly.
-	//I can guess why this is so, but honestly, this was just found through playing around,
-	//and I don't really *know* why this is so.  Note that anything that break up the thread control,
-	//like an alert, or putting the code outside of this function seems to work.
-	setTimeout(function() {Object.defineProperty(obj, prop, descriptor);}, 0);
+	setTimeout(function() {
+	    
+	    var descriptor = Object.getOwnPropertyDescriptor(obj, prop);
+	    delete obj[prop]; 
+	    originalSetAttribute(prop, value);
+	    obj.className=obj.className;
+	    //Without the "setTimeout", the style changes don't apply correctly.
+	    //I can guess why this is so, but honestly, this was just found through playing around,
+	    //and I don't really *know* why this is so.  Note that anything that break up the thread control,
+	    //like an alert, or putting the code outside of this function seems to work.
+	    setTimeout(function() {Object.defineProperty(obj, prop, descriptor);}, 0);
+	}, 0);
+
     }
 
     obj.setAttribute = function (name, value) {

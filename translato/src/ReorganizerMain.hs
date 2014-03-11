84a2d11a45bc7583a1aac3881608cfcc829f8c09
jim <jim>
2014-03-11 16:38:41 -0700

    Adding usage message.

--- translato/src/ReorganizerMain.hs
+++ translato/src/ReorganizerMain.hs
@@ -9,11 +9,13 @@ import System.Environment
 import Paths_translato
 
 main = do
-  [userAgent] <- getArgs
-  specFilePath <- getDataFileName "spec"
-  let shimDir = "/home/jim/GlowApps/html5/shims"
-  result <- reorganize shimDir userAgent =<< TL.getContents
-  case result of
-    Right reorganized -> putStrLn $ TL.unpack reorganized
-    Left err -> error (show err)
-    
+  args <- getArgs
+  case args of
+    [userAgent] -> do
+      specFilePath <- getDataFileName "spec"
+      let shimDir = "/home/jim/GlowApps/html5/shims"
+      result <- reorganize shimDir userAgent =<< TL.getContents
+      case result of
+        Right reorganized -> putStrLn $ TL.unpack reorganized
+        Left err -> error (show err)
+    _ -> error "Usage:\n  reorganizo <userAgent>"

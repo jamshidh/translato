66b06cf8a20023677cfc9ba437f573e72a3749a4
jim <jim>
2014-03-21 15:03:46 -0700

    Adding proper credit.

--- html5/shims/dataset/lib/dataset.js
+++ html5/shims/dataset/lib/dataset.js
@@ -1,22 +1,6 @@
 /**
- * Add dataset support to elements
- * No globals, no overriding prototype with non-standard methods, 
- *   handles CamelCase properly, attempts to use standard 
- *   Object.defineProperty() (and Function bind()) methods, 
- *   falls back to native implementation when existing
- * Inspired by http://code.eligrey.com/html5/dataset/ 
- *   (via https://github.com/adalgiso/html5-dataset/blob/master/html5-dataset.js )
- * Depends on Function.bind and Object.defineProperty/Object.getOwnPropertyDescriptor (shims below)
- * Licensed under the X11/MIT License
+ * Based on code from https://gist.github.com/brettz9/4093766#file_html5_dataset.js
 */
-
-// Removed a few JSLint options as Notepad++ JSLint validator complaining and 
-//   made comply with JSLint; also moved 'use strict' inside function
-/*jslint white: true, undef: true, plusplus: true,
-  bitwise: true, regexp: true, newcap: true, maxlen: 90 */
-
-
-// Begin dataset code
 
 var propDescriptor = {
     enumerable: true,

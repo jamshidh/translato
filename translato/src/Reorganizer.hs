19573c0da629000a53fbb1a65607a97b9a3e20c8
jim <jim>
2014-03-21 18:18:50 -0700

    Improved error reporting.

--- translato/src/Reorganizer.hs
+++ translato/src/Reorganizer.hs
@@ -90,9 +90,18 @@ libToParserSpec (CSSLib _) = "css"
 --Gets needed libs in a single file, but don't add libs needed by those libs
 documentToLibs::XML.Document->[ShimName]
 documentToLibs doc = 
-      (ShimName <$> ((descendant $ fromDocument doc) >>= element "shimLib" >>= attribute "name"))
-      
-  
+      (ShimName <$> ((descendant $ fromDocument doc) >>= element "shimLib" >>= getNameAttribute))
+      where --All of these helper functions are for error checking.  We need to make sure that each shimLib tag has one and only one "@name" tag.
+        getNameAttribute::Cursor->[T.Text]
+        getNameAttribute c = node2Name $ node c 
+        node2Name::XML.Node->[T.Text]
+        node2Name (XML.NodeElement XML.Element{XML.elementAttributes=atts}) = [uniqueNameValue $ M.toList atts]
+        uniqueNameValue::[(XML.Name, T.Text)]->T.Text
+        uniqueNameValue [] = error "A <shimLib> tag is missing its 'name' attribute."
+        uniqueNameValue [("name", value)] = value
+        uniqueNameValue atts = error ("unexpected attributes in <shimLib>: [" ++ intercalate ", " (showAttribute <$> atts) ++ "]\n----Remember, only 'name' is allowed.")
+        showAttribute::(XML.Name, T.Text)->String
+        showAttribute (n, v)=T.unpack (XML.nameLocalName n) ++ "='" ++ T.unpack v ++ "'"
 
 
 modify::[ShimConfig]->String->XML.Document->IO XML.Document

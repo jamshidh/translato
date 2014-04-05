7de32a223e5194883b423e07432092502a19e78e
jim <jim>
2014-04-04 20:06:09 -0700

    Adding the range input shim.

---
+++ html5/shims/addRangeInput/lib/addRangeInput.js
@@ -0,0 +1,85 @@
+var global_gripper;
+var global_bar;
+var global_input;
+
+function getRect(target) {
+    var rect = target.getBoundingClientRect();
+    
+    //TODO- standardize this and put it in a shim
+    var scrollX = (window.pageXOffset !== undefined) ? window.pageXOffset : (document.documentElement || document.body.parentNode || document.body).scrollLeft;
+    var scrollY = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
+    
+    rect.left += scrollX;
+    rect.right += scrollX;
+    rect.top += scrollY;
+    rect.bottom += scrollY;
+    
+    return rect;
+}
+
+function valOrDefault(x, def) {
+    if (!x) return def;
+    return x;
+}
+
+function mouseDown(theId, event) {
+    document.onmousemove = mouseMove;
+    document.onmouseup = mouseUp;
+    global_gripper = document.getElementById(theId+"_gripper");
+    global_bar = document.getElementById(theId+"_bar");
+    global_input = document.getElementById(theId);
+}
+function barPress(theId, event) {
+    global_gripper = document.getElementById(theId+"_gripper");
+    global_bar = document.getElementById(theId+"_bar");
+    global_input = document.getElementById(theId);
+    mouseMove(event);
+}
+function mouseMove(e) {
+    var min = parseFloat(global_input.min);
+    var max = parseFloat(global_input.max);
+    var step = parseFloat(valOrDefault(global_input.step, "1"));
+    
+    var event = e || window.event;
+    barRect = getRect(global_bar);
+    
+    global_input.value = (max - min) * (event.clientX - barRect.left)/(barRect.right - barRect.left) + min;
+    
+    if (global_input.value < min) global_input.value = min;
+    if (global_input.value > max) global_input.value = max;
+    
+    global_input.value = Math.round(global_input.value/step)*step;
+    
+    placeGripper();
+    
+}
+
+function placeThisGripper(theId) {
+    global_gripper = document.getElementById(theId+"_gripper");
+    global_bar = document.getElementById(theId+"_bar");
+    global_input = document.getElementById(theId);
+    placeGripper();
+}
+
+function placeGripper() {
+    
+    barRect = getRect(global_bar);
+    gripperRect = getRect(global_gripper)
+    
+    gripperHeight = gripperRect.bottom - gripperRect.top;
+    gripperWidth = gripperRect.right - gripperRect.left;
+    
+    barMiddleY = (barRect.top + barRect.bottom)/2;
+    
+    
+    global_gripper.style.top = (barMiddleY - gripperHeight/2.0) + "px";
+    
+    global_gripper.style.left = (global_input.value - global_input.min)/(global_input.max - global_input.min) * (barRect.right - barRect.left) + barRect.left - gripperWidth/2 + "px";
+    
+    
+    
+}
+function mouseUp() {
+    document.onmousemove = null;
+}
+
---
+++ html5/shims/addRangeInput/translate.xsl
@@ -0,0 +1,47 @@
+<?xml version="1.0" ?>
+
+<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+  <xsl:template match="@*|node()">
+    <xsl:copy>
+      <xsl:apply-templates select="*|@*|text()" />
+    </xsl:copy>
+  </xsl:template>
+
+  <xsl:template match="input[attribute[@name='type' and @value='range']]">
+    <xsl:variable name="theId">
+      <xsl:choose>
+	<xsl:when test="attribute[@name='id']"><xsl:value-of select="attribute[@name='id']/@value" /></xsl:when>
+	<xsl:otherwise><xsl:value-of select="generate-id()" /></xsl:otherwise>
+      </xsl:choose>
+    </xsl:variable>
+
+
+    <shimLib name="addRangeInput" />
+    <xsl:copy>
+      <attribute name="id" value="{$theId}" />
+      <attribute name="style" value="display: none;" />
+      <xsl:apply-templates select="*|@*|text()" />
+    </xsl:copy>
+    <div>
+      <div>
+	<attribute name="id" value="{concat($theId,'_bar')}" />
+	<attribute name="class" value="rangeInputBar" />
+	<attribute name="onmousedown" value='{concat(concat("barPress(&apos;", $theId), "&apos;, event);")}' />
+      </div>
+      <span>
+	<attribute name="id" value="{concat($theId,'_gripper')}" />
+	<attribute name="class" value="rangeInputGripper" />
+	<attribute name="onmousedown" value='{concat(concat("mouseDown(&apos;", $theId), "&apos;, event);")}' />
+      </span>
+    </div>
+    <script>
+      <expressionCommand>
+	<function>
+	  <variable name="placeThisGripper" />
+	  <string value="{$theId}" />
+	</function>
+      </expressionCommand>
+    </script>
+  </xsl:template>
+
+</xsl:stylesheet>

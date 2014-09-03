6a6350cb74bf5b3601b207251b17679ad5fa07c9
jim <jim>
2014-09-02 17:45:18 -0700

    Cleanup.

--- html5/shims/addServerFunction/lib/serverFunction.js
+++ html5/shims/addServerFunction/lib/serverFunction.js
@@ -5,11 +5,9 @@ function callServer(funcName, params) {
     var content = JSON.stringify({funcName:funcName, params:[].slice.apply(params)});
 
     request.open('POST', location.href.substr(0, location.href.lastIndexOf(".")) + ".sjs", false);  // `false` makes the request synchronous
-    request.setRequestHeader("Content-length", content.length);
     request.send(content);
     
     if (request.status === 200) {
-	console.log(request.responseText);
 	return request.responseText;
     }
     
